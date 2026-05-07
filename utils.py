4a610423164afd8671f6f88ea257c6b683361222
Gaibo Zhang <gaibozhang@yahoo.com>
2026-05-07 18:56:29 -0400

    add some example scenarios for fun1

--- utils.py
+++ utils.py
@@ -39,6 +39,29 @@ def epic_chance(total_size: int, n_desired: int, draw_size: int) -> dict[int, fl
         6: 0.0%
         7: 0.0%"
 
+        Example 3:
+        Scenario: "Woah, I just pulled BigTime Hazard AND epic Sneijder in my first 10-draw! I must be the 
+                   luckiest person on the planet - I wonder what were the chances of pulling 2 epics including 
+                   Hazard? It was a 250-pack (with a free roll) that had 7 epics including the BigTime."
+        Analysis: This actually happened to me lol. We start by calculating the chance of pulling any 2 epics:
+            Usage: epic_chance(250-1, 7, 10)
+            Output: {0: 0.7478759630652251,
+                     1: 0.22468376572775,
+                     2: 0.025925049891663464,
+                     3: 0.0014709248165482817,
+                     4: 4.362912591456768e-05,
+                     5: 6.627208999681165e-07,
+                     6: 4.640902660841153e-09,
+                     7: 1.109600158001471e-11}
+                  So that's a 2.59% (~1 in 40) chance to start. But we were luckier than that - how many of these 
+                  epic 2-combos include Hazard specifically? There are 7choose2=21 total combos, 6 of which include 
+                  Hazard (just fix Hazard and then cycle through the other 6). That's a 2/7 chance on top of the 
+                  2.59% - multiplying for concurrency we get 0.74%, or 1 in 135!
+        Answer: So I am very lucky, but not the luckiest in the world! On average, 1 in every 135 players who 
+                drew 10 experienced the same unforgettable Big Time double walkout animation. And as we know, 
+                there are more than one million "serious" players competing in Divisions on mobile - if 1 million 
+                went for the pack, then we'd estimate 1e6/135=7407 people had the same luck!
+
     :param int total_size: Number of remaining possible players left in the pack. 
         e.g. If it's a 250-player pack, and you already used the free chance, then enter "249".
     :param int n_desired: Number of players you actually care about still left in the pack. 
