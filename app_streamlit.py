58e80e9c23093b3f40a317c7194ef3d6f69fb828
Gaibo Zhang <gaibozhang@yahoo.com>
2026-05-15 13:48:42 -0400

    checkpoint for app_streamlit.py - usable, now just adding bells and whistles like scrollable chat and sidebar!

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,10 +1,12 @@
 import streamlit as st
-from utils import epic_chance, text_to_markdown
+from utils import epic_chance, generate_message
 from contextlib import redirect_stdout
 import io
-import re
+import pandas as pd
+
 
 # GUI heading
+st.image('assets/efootball-banner-cropped.jpeg')    # TODO: Put banner, squares, and pull on GitHub
 # SEO stuff; google "site:packprob.streamlit.app" to check this indexed meta info
 st.set_page_config(page_title="packprob")   # SEO
 st.title("packprob")
@@ -13,6 +15,10 @@ st.header("eFootball Pack Probability Calculator")  # SEO
 st.text("Calculate the probability of actually getting the player(s) you want from an eFootball pack draw!\n"
         "Yeah, it'll be less than you think...")    # SEO
 
+
+# TODO: Sidebar with README from GitHub
+
+
 # GUI base layout horizontal - left for inputs, right for outputs
 base_cont = st.container(horizontal=True)
 left_cont = base_cont.container(border=True)
@@ -20,20 +26,24 @@ right_cont = base_cont.container(border=True)
 # Alternate method for doing horizontal layout, maybe more mobile-friendly?
 # left_cont, right_cont = st.columns(2, border=True)
 
+
 with left_cont:
     # Fields for inputs from human user
     total_size = \
-        st.number_input("Total pack size (remaining):", 
+        st.number_input("Total pack size (remaining) 🟥:", 
                         value=250, min_value=1, step=10,
-                        key='total_size')   # TODO: Add hover tooltip description
+                        key='total_size',
+                        help="e.g. If it's a 250-player pack, and you already used the free chance, then enter \"249\".")
     n_desired = \
-        st.number_input("# of cards you like:", 
+        st.number_input("# of cards you want 🟦:", 
                         value=7, min_value=1, max_value=total_size,
-                        key='n_desired')
+                        key='n_desired',
+                        help="e.g. If there are 7 epics and you don't really need 3 of them, then enter \"4\".")
     draw_size = \
-        st.number_input("# of cards you'll draw:", 
+        st.number_input("# of cards you'll draw 🟪:", 
                         value=10, min_value=1, max_value=total_size,
-                        key='draw_size')
+                        key='draw_size',
+                        help="e.g. If you're dropping 900 coins to get the discounted 10 chances, then enter \"10\".")
 
     # Input validation
     # Well actually, st.number_input() ensures inputs always legal!
@@ -48,21 +58,50 @@ with left_cont:
         with st.spinner("Running the numbers..."):
             try:
                 # Plug inputs into the backend
-                f = io.StringIO()   # Capture all the print statements
-                with redirect_stdout(f):
-                    result = epic_chance(total_size, n_desired, draw_size)  # TODO: Make epic_chance() prints Markdown-friendly
-                f_prints = f.getvalue()     # Collected print strings
-                f_prints_markdown = text_to_markdown(f_prints)  # Convert the newlines to Markdown?
-                st.session_state['output_markdown'] = f_prints_markdown  # TODO: Put output Markdown into session state
+
+                # # Method 1: redirect_stdout() to pipe print statements
+                # # Problem: Can't cache epic_chance() outputs, because we'd need it to print every time!
+                # f = io.StringIO()   # Capture all the print statements
+                # with redirect_stdout(f):
+                #     pulls_chance_dict = epic_chance(total_size, n_desired, draw_size, verbose=True)
+                # message_md = f.getvalue()     # Collected print strings (assume already Markdown!)
+
+                # Method 2: Take only the output dict of epic_chance(), then construct Markdown here
+                pulls_chance_dict = epic_chance(total_size, n_desired, draw_size, verbose=False)
+                message_md = generate_message(total_size, n_desired, draw_size, pulls_chance_dict, print_dict=False)
+
+                st.session_state['output_markdown'] = message_md
+                st.session_state['output_dict'] = pulls_chance_dict
             except Exception as e:
                 st.exception(e)
     
-    # TODO: Add screenshot of an eFootball campaign with colored squares for reference
+    # Example screenshot of an eFootball campaign with colored squares highlighting what to input
+    st.image('assets/pack-info-squares.png')
+    st.image('assets/pack-desired-squares.png')
+
 
 with right_cont:
-    if 'output_markdown' not in st.session_state:    # Check existence of result
+    if 'output_dict' not in st.session_state:  # Check existence of result
         st.info("Waiting for your numbers...")
     else:
-        # Print of the final results for the human user
+        # Print the final results for the human user
         # TODO: Make a chat message style print log, so we can see history!
-        st.success(st.session_state.output_markdown)
+        #       Requires using st.session_state to keep full list of printouts...
+        # with st.container(border=True):
+        with st.chat_message("assistant"):
+            # The explanation
+            # st.text(repr(st.session_state.output_markdown))   # For debugging Markdown newlines!
+            st.success(st.session_state.output_markdown)
+
+            # The table of probabilities
+            out_ser = pd.Series(st.session_state.output_dict)
+            out_ser.index.name = 'Pulls'
+            out_ser.name = 'Chance'
+            st.dataframe(
+                out_ser,
+                column_config={
+                    'Chance': st.column_config.NumberColumn(
+                        format='percent'
+                    )
+                }
+            )
